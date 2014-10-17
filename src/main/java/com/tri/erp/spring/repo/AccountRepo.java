63db5003285e5d769330d68ede214a9674d03d4d
Arjay Adong <arjayads@gmail.com>
2014-10-17 17:22:07 +0800

    change account columns

--- src/main/java/com/tri/erp/spring/repo/AccountRepo.java
+++ src/main/java/com/tri/erp/spring/repo/AccountRepo.java
@@ -20,14 +20,14 @@ public interface AccountRepo extends JpaRepository<Account, Integer> {
     public List<Account> findByIdNotInOrderByTitleAsc(Integer... accountId);
 
     @Transactional
-    @Query(value = "SELECT accounts.acct_id, " +
-            "accounts.title, " +
+    @Query(value = "SELECT Account.id, " +
+            "Account.title, " +
             "segment_accounts.acct_code, " +
             "account_type.description " +
-            "FROM accounts " +
-            "JOIN segment_accounts ON accounts.acct_id = segment_accounts.acct_id " +
-            "LEFT JOIN account_type ON accounts.acct_type_id = account_type.id " +
+            "FROM Account " +
+            "JOIN segment_accounts ON Account.id = segment_accounts.acct_id " +
+            "LEFT JOIN account_type ON Account.FK_accountTypeId = account_type.id " +
             "WHERE segment_accounts.bus_seg_id IN (:segmentIds) " +
-            "ORDER BY accounts.code ASC, segment_accounts.acct_code ASC", nativeQuery = true)
+            "ORDER BY Account.code ASC, segment_accounts.acct_code ASC", nativeQuery = true)
     public List<Object[]> findBySegmentIds(@Param("segmentIds") List<String> segmentIds);
 }
