3c6542c7c4aad3d93869e7faf0b59176dbc0959c
Arjay Adong <arjayads@gmail.com>
2014-11-13 13:16:55 +0800

    item service

--- src/main/java/com/tri/erp/spring/service/interfaces/ItemService.java
+++ src/main/java/com/tri/erp/spring/service/interfaces/ItemService.java
@@ -2,9 +2,14 @@ package com.tri.erp.spring.service.interfaces;
 
 import com.tri.erp.spring.model.Item;
 import com.tri.erp.spring.model.SlEntity;
+import com.tri.erp.spring.reponse.CreateResponse;
+import org.springframework.context.MessageSource;
+import org.springframework.validation.BindingResult;
 
 import java.util.List;
 
 public interface ItemService {
     public List<Item> findAll();
+    public CreateResponse processUpdate(Item item, BindingResult bindingResult, MessageSource messageSource);
+    public CreateResponse processCreate(Item item, BindingResult bindingResult, MessageSource messageSource);
 }
