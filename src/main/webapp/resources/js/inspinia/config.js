ae4d35cb4b7e92ab80d5147633deb7880652ed86
Arjay Adong <arjayads@gmail.com>
2014-12-02 13:29:15 +0800

    reload menu view when clicked

--- src/main/webapp/resources/js/inspinia/config.js
+++ src/main/webapp/resources/js/inspinia/config.js
@@ -102,10 +102,43 @@ function config($stateProvider, $urlRouterProvider) {
         })
 }
 
+function decorator($provide) {
+    $provide.decorator('$state', function ($delegate) {
+
+        // let's locally use 'state' name
+        var state = $delegate;
+
+        // let's extend this object with new function
+        // 'baseGo', which in fact, will keep the reference
+        // to the original 'go' function
+        state.baseGo = state.go;
+
+        // here comes our new 'go' decoration
+        var go = function (to, params, options) {
+            options = options || {};
+
+            // only in case of missing 'reload'
+            // append our explicit 'true'
+            if (angular.isUndefined(options.reload)) {
+
+                options.reload = true;
+            }
+
+            // return processing to the 'baseGo' - original
+            this.baseGo(to, params, options);
+        };
+
+        // assign new 'go', right now decorating the old 'go'
+        state.go = go;
+
+        return $delegate;
+    });
+}
 
 angular
     .module('inspinia')
     .config(config)
+    .config(decorator)
     .run(function($rootScope, $state) {
         $rootScope.$state = $state;
     });
