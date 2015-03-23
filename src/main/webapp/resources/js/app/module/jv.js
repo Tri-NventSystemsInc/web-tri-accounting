2c5b2bfdb90b7ffdc116cc817a07bb87e8e74cfa
megeh09 <megeh09@gmail.com>
2015-03-23 17:31:06 +0800

    JV initials.

--- src/main/webapp/resources/js/app/module/jv.js
+++ src/main/webapp/resources/js/app/module/jv.js
@@ -1,3 +1,17 @@
 /**
  * Created by TSI Admin on 3/23/2015.
  */
+var jvApp = angular.module('jv', [
+    'jQueryFnWrapperService',
+    'errorHandlerService',
+    'cmnFormErrorApp',
+    'utilService'
+]);
+
+jvApp.controller('addEditCvCtrl', ['$scope', '$stateParams', '$http', 'errorToElementBinder', 'csrf',
+    function($scope, $stateParams, $http, errorToElementBinder, csrf) {
+
+        $scope.title = 'Create journal voucher';
+        $scope.save = 'Save';
+        $scope.showForm = true;
+    }]);
