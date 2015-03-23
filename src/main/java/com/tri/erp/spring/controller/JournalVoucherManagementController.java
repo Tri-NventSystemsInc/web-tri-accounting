fe4c0f2278c7b14c6ca0b934fdcbcd3d91a485ba
megeh09 <megeh09@gmail.com>
2015-03-23 17:30:53 +0800

    JV initials.

---
+++ src/main/java/com/tri/erp/spring/controller/JournalVoucherManagementController.java
@@ -0,0 +1,7 @@
+package com.tri.erp.spring.controller;
+
+/**
+ * Created by TSI Admin on 3/23/2015.
+ */
+public class JournalVoucherManagementController {
+}
---
+++ src/main/webapp/resources/js/app/module/jv.js
@@ -0,0 +1,3 @@
+/**
+ * Created by TSI Admin on 3/23/2015.
+ */
